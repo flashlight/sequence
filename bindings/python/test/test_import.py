e3165647cc3955c1d355eb50b40006be0d837368
jacobkahn <jacobkahn1@gmail.com>
2022-11-16 13:39:32 -0800

    Improve import tests for Python bindings

--- bindings/python/test/test_import.py
+++ bindings/python/test/test_import.py
@@ -1,16 +1,39 @@
 """
 Copyright (c) Facebook, Inc. and its affiliates.
-
 This source code is licensed under the MIT-style license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+import logging
 import os
+import unittest
 
 
-def test_import_lib_audio():
-    from flashlight.lib.audio import feature as fl_feat
+class ImportTestCase(unittest.TestCase):
+    def test_import_lib_sequence(self):
+        from flashlight.lib.sequence import criterion
+        from flashlight.lib.sequence.criterion import (
+            CpuForceAlignmentCriterion,
+            CpuFullConnectionCriterion,
+            CpuViterbiPath,
+            CriterionScaleMode,
+        )
+
+        if os.getenv("USE_CUDA", "OFF").upper() not in [
+            "OFF",
+            "0",
+            "NO",
+            "FALSE",
+            "N",
+        ]:
+            from flashlight.lib.sequence.flashlight_lib_sequence_criterion import (
+                CudaForceAlignmentCriterion,
+                CudaFullConnectionCriterion,
+                CudaViterbiPath,
+            )
+        else:
+            logging.info("Flashlight Sequence bindings built without CUDA")
 
 
-def test_import_lib_sequence():
-    from flashlight.lib.sequence import criterion as fl_crit
+if __name__ == "__main__":
+    unittest.main()
