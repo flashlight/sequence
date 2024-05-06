158f4670379a6885bb5d7f7ee8dabd4fb548d4c5
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-05-06 16:06:22 -0700

    deeplearning/projects/flashlight-sequence/bindings/python:lib_sequence_criterion
    Reviewed By: AishwaryaSivaraman
    
    Differential Revision: D57014038
    
    fbshipit-source-id: c44de35c6b46cafd36d0019ceadaa53cfe9339ed

--- bindings/python/flashlight/lib/sequence/criterion.py
+++ bindings/python/flashlight/lib/sequence/criterion.py
@@ -6,7 +6,7 @@ This source code is licensed under the MIT-style license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-from .flashlight_lib_sequence_criterion import (
+from .flashlight_lib_sequence_criterion import (  # @manual
     CpuForceAlignmentCriterion,
     CpuFullConnectionCriterion,
     CpuViterbiPath,
